e1c70222325b5d142c5ae38fdf9c18dea400599d
Roman <roman.vysotsky@fh-bingen.de>
2017-05-25 10:06:58 +0200

    Publish, Unpublish, Subscribe, Unsubscribe Methoden Entsprechend Buttons ListView für received messages ungetestet

--- NearbyVortragApp/app/src/main/java/com/example/johannes/nearbyvortragapp/MainActivity.java
+++ NearbyVortragApp/app/src/main/java/com/example/johannes/nearbyvortragapp/MainActivity.java
@@ -7,26 +7,118 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
     GoogleApiClient mGoogleApiClient;
+    Message mActiveMessage;
+    MessageListener mMessageListener;
+
+    EditText msg;
+    Button pub, unpub, sub, unsub;
+    ListView listView;
+
+    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+    ArrayList<String> listItems;
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        msg = (EditText) findViewById(R.id.msg);
+        pub = (Button) findViewById(R.id.pub);
+        unpub = (Button) findViewById(R.id.unpub);
+        sub = (Button) findViewById(R.id.sub);
+        unsub = (Button) findViewById(R.id.unsub);
+
+        initListView();
+        setListeners();
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(Nearby.MESSAGES_API)
                 .addConnectionCallbacks(this)
                 .enableAutoManage(this, this)
                 .build();
+
+    }
+
+    private void initListView(){
+        listView = (ListView) findViewById(R.id.list);
+        listItems = new ArrayList<>();
+        adapter=new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        listView.setAdapter(adapter);
+    }
+
+    private void setListeners(){
+        pub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                publish(msg.getText().toString()); // Publish message from EditText as mActiveMessage
+            }
+        });
+        unpub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                unpublish(); // Unpublish active message
+            }
+        });
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                subscribe(); // Subscribe to receive active messages
+            }
+        });
+        unsub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                unsubscribe(); // Unsubscribe from active messages
+            }
+        });
+
+        mMessageListener = new MessageListener() {
+            @Override
+            public void onFound(Message message) {
+                String messageAsString = new String(message.getContent());
+                Log.d("FOUND", "Found message: " + messageAsString);
+                addItemToListView(messageAsString);
+            }
+
+            @Override
+            public void onLost(Message message) {
+                String messageAsString = new String(message.getContent());
+                Log.d("LOST", "Lost sight of message: " + messageAsString);
+                removeItemFromListView(messageAsString);
+            }
+        };
+    }
+
+    public void addItemToListView(String item) {
+        listItems.add(item);
+        adapter.notifyDataSetChanged();
+    }
+
+    public void removeItemFromListView(String item){
+        listItems.remove(item);
+        adapter.notifyDataSetChanged();
     }
 
     @Override
@@ -34,6 +126,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         super.onStart();
         mGoogleApiClient.connect();
 
+        //publish("Hello World");
+        //subscribe();
     }
 
     @Override
@@ -60,18 +154,27 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         super.onStop();
     }
 
-    private static int[] NETWORK_TYPES = {ConnectivityManager.TYPE_WIFI,
-            ConnectivityManager.TYPE_ETHERNET};
+    private void publish(String message) {
+        Log.i("PUB", "Publishing message: " + message);
+        mActiveMessage = new Message(message.getBytes());
+        Nearby.Messages.publish(mGoogleApiClient, mActiveMessage);
+    }
 
-    private boolean isConnectedToNetwork() {
-        ConnectivityManager connManager =
-                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        for (int networkType : NETWORK_TYPES) {
-            NetworkInfo info = connManager.getNetworkInfo(networkType);
-            if (info != null && info.isConnectedOrConnecting()) {
-                return true;
-            }
+    private void unpublish() {
+        Log.i("UNPUB", "Unpublishing.");
+        if (mActiveMessage != null) {
+            Nearby.Messages.unpublish(mGoogleApiClient, mActiveMessage);
+            mActiveMessage = null;
         }
-        return false;
+    }
+
+    private void subscribe() {
+        Log.i("SUB", "Subscribing.");
+        Nearby.Messages.subscribe(mGoogleApiClient, mMessageListener);
+    }
+
+    private void unsubscribe() {
+        Log.i("UNSUB", "Unsubscribing.");
+        Nearby.Messages.unsubscribe(mGoogleApiClient, mMessageListener);
     }
 }
